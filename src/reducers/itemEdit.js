cd650139a1746c77f465362db317772bae4e9f5a
Ankhang1723 <doducan.ltu14@gmail.com>
2020-09-30 17:24:09 +0700

    update reactjs redux

---
+++ src/reducers/itemEdit.js
@@ -0,0 +1,24 @@
+
+import * as types from './../constants/ActionTypes'
+
+
+var initialState = {
+	id : '',
+	name : '',
+	status : false
+};
+
+var myReducer = (state = initialState, action) => {
+	switch(action.type) {
+		case types.EDIT_ITEM:
+		console.log(action);	
+			return action.task;		
+
+		default: 
+			return state;
+	}
+
+}
+
+
+export default myReducer;
